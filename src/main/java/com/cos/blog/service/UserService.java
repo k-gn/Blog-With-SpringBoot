0aa8eba2e15046909b4fa943c74d5c63d5cc44c8
k-gn <rlarbska97@gmail.com>
2021-06-23 20:57:52 +0900

    회원가입

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -6,11 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+// service 가 필요한 이유
+// 1. 트랜잭션 관리
+// 2. 하나의 기능으로 처리 (서비스화, 여러 기능이 하나로 모인게 하나의 기능으로 처리될 수도 있다.)
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepo;
+
+    @Transactional
+    public int save(User user) {
+        try {
+            userRepo.save(user);
+            return 1;
+        }catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("회원가입 : " + e.getMessage());
+        }
+        return -1;
+    }
 
     @Transactional
     public User update(Long id, User reqUser) {
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,6 +1,6 @@
 let index = {
     init: function () {
-        $("#btn-save").on("click", () => {
+        $("#btn-save").on("click", () => { // this 바인딩을 위해 화살표함수로 사용
             this.save();
         });
     },
@@ -15,7 +15,19 @@ let index = {
 
         console.log(data);
 
-        $.ajax().done().fail();
+        $.ajax({
+            type: "POST",
+            url: "/blog/api/user",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json" // 응답이 왔을 때 생긴게 json 이라면 javascript 오브젝트로 변환 (생략해도 서버가 json 으로 보내면 알아서 받을 수 있다.)
+        }).done((resp) => {
+            console.log(resp);
+            alert("회원가입이 완료되었습니다.");
+            location.href="/blog"
+        }).fail((error) => {
+            console.log(error);
+        });
     }
 }
 
