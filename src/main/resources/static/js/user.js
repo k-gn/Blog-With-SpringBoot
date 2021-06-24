d36481f74479440499efcc8d57cf022ce5e87dbd
k-gn <rlarbska97@gmail.com>
2021-06-24 11:58:13 +0900

    경로 변경

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -20,14 +20,14 @@ let index = {
 
         $.ajax({
             type: "POST",
-            url: "/blog/api/user",
+            url: "/api/user",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json" // 응답이 왔을 때 생긴게 json 이라면 javascript 오브젝트로 변환 (생략해도 서버가 json 으로 보내면 알아서 받을 수 있다.)
         }).done((resp) => {
             console.log(resp);
             alert("회원가입이 완료되었습니다.");
-            location.href="/blog"
+            location.href="/"
         }).fail((error) => {
             console.log(error);
         });
@@ -44,14 +44,14 @@ let index = {
 
         $.ajax({
             type: "POST",
-            url: "/blog/api/user/login",
+            url: "/api/user/login",
             data: JSON.stringify(data),
             contentType: "application/json; charset=utf-8",
             dataType: "json" // 응답이 왔을 때 생긴게 json 이라면 javascript 오브젝트로 변환 (생략해도 서버가 json 으로 보내면 알아서 받을 수 있다.)
         }).done((resp) => {
             console.log(resp);
             alert("로그인이 완료되었습니다.");
-            location.href="/blog"
+            location.href="/"
         }).fail((error) => {
             console.log(error);
         });
