31698f53a5b88b08369a74c64af958da3ccbba27
k-gn <rlarbska97@gmail.com>
2021-06-25 09:50:42 +0900

    kakao login 추가

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.cos.blog.controller;
 
-import com.cos.blog.auth.PrincipalDetail;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
@@ -22,4 +28,38 @@ public class UserController {
     public String updateForm() {
         return "user/updateForm";
     }
+
+    @GetMapping("/auth/kakao/callback")
+    @ResponseBody
+    public String kakaoCallback(String code) {
+
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "3bb60d70dee580c716f767af0de9c912");
+        params.add("redirect_uri", "http://localhost:8000/auth/kakao/callback");
+        params.add("code", code);
+
+        // HttpHeader 와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String, String>> kakaoRequest = new HttpEntity<>(params, headers);
+
+        // Http 요청
+        ResponseEntity<String> response = rt.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                kakaoRequest,
+                String.class
+        );
+
+        return "카카오 인증 완료 " + response;
+    }
 }
+//https://kauth.kakao.com/oauth/token
+//        grant_type=authorization_code
+//        client_id=3bb60d70dee580c716f767af0de9c912
+//        redirect_uri=http://localhost:8000/auth/kakao/callback
+//        code=code
